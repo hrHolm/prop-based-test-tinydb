935fda5e1e88016faa3b1695fda13b42963b3d3e
Frederik Holm <hollm@live.dk>
2020-04-20 20:47:22 +0200

    added simple insert remove statemachine that passes
    however it takes between 30-60 seconds to run. This is due to many invalid cases -> should be solvable with better preconditions!

---
+++ test_model.py
@@ -0,0 +1,43 @@
+from tinydb import TinyDB
+from tinydb.storages import MemoryStorage
+from hypothesis.strategies import integers, dictionaries, text
+from hypothesis import given, settings, event
+from hypothesis.stateful import Bundle, RuleBasedStateMachine, rule, precondition
+from collections import defaultdict
+
+class TinyDBComparison(RuleBasedStateMachine):
+    def __init__(self):
+        super(TinyDBComparison, self).__init__()
+        self.database = TinyDB(storage=MemoryStorage) # sut
+        self.model = defaultdict(set)
+
+    ids = Bundle("ids")
+    documents = Bundle("documents")
+
+    '''
+    @rule(target=ids, k=st.binary())
+    def add_key(self, k):
+        return k
+    '''
+    @rule(target=documents, v=dictionaries(keys=text(), values=integers()))
+    def add_value(self, v):
+        return v
+    
+    @rule(k=ids, v=documents)
+    def insert(self, v):
+        d_id = self.database.insert(v) # TinyDB calculates ID when inserting
+        self.model[d_id].add(v)
+
+    @precondition(lambda self: len(self.model) > 0) # TODO: find better precondition
+    @rule(k=ids, v=documents)
+    def remove(self, k, v):
+        self.model[k].discard(v)
+        self.database.remove(doc_id=k)
+
+
+    @rule(k=ids)
+    def values_agree(self, k):
+        '''Property'''
+        assert set(self.database.get(doc_id=k)) == self.model[k]
+
+TestDBComparison = TinyDBComparison.TestCase
