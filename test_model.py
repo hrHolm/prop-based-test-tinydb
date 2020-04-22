f2d5e745cdff80f5434a28c9e427e5e015bab7c2
Phillip Olsen <phillip-olsen@hotmail.com>
2020-04-22 09:57:00 +0200

    Updated examples
    added get_all / get specific id

--- test_model.py
+++ test_model.py
@@ -5,10 +5,11 @@ from hypothesis import given, settings, event
 from hypothesis.stateful import Bundle, RuleBasedStateMachine, rule, precondition
 from collections import defaultdict
 
+
 class TinyDBComparison(RuleBasedStateMachine):
     def __init__(self):
         super(TinyDBComparison, self).__init__()
-        self.database = TinyDB(storage=MemoryStorage) # sut
+        self.database = TinyDB(storage=MemoryStorage)  # sut
         self.model = defaultdict(set)
 
     ids = Bundle("ids")
@@ -19,25 +20,36 @@ class TinyDBComparison(RuleBasedStateMachine):
     def add_key(self, k):
         return k
     '''
+
     @rule(target=documents, v=dictionaries(keys=text(), values=integers()))
     def add_value(self, v):
         return v
-    
+
     @rule(k=ids, v=documents)
     def insert(self, v):
-        d_id = self.database.insert(v) # TinyDB calculates ID when inserting
+        d_id = self.database.insert(v)  # TinyDB calculates ID when inserting
         self.model[d_id].add(v)
 
-    @precondition(lambda self: len(self.model) > 0) # TODO: find better precondition
+    @precondition(lambda self: len(self.model) > 0)  # TODO: find better precondition
     @rule(k=ids, v=documents)
     def remove(self, k, v):
         self.model[k].discard(v)
         self.database.remove(doc_id=k)
 
+    @precondition(lambda self: len(self.model) > 0)  # TODO: find better precondition
+    @rule()
+    def get_all(self):
+        assert len(self.model) == len(self.database.all())
+
+    @precondition(lambda self: len(self.model) > 0)  # TODO: find better precondition
+    @rule(k=ids)
+    def search_id(self, k):
+        assert self.model[k] == self.database.search(id == k)
 
     @rule(k=ids)
     def values_agree(self, k):
         '''Property'''
         assert set(self.database.get(doc_id=k)) == self.model[k]
+
 
 TestDBComparison = TinyDBComparison.TestCase
