4e81fdbf3be0e6910680a17a3f6750fa142c16c0
Emil Johansen <emil@florint.dk>
2020-05-22 14:53:16 +0200

    Made a no error JSON storage version for coverage testing.

--- roundtrip.py
+++ roundtrip.py
@@ -38,10 +38,12 @@ def test_roundtrip(thing):
     db.insert(thing)
     assert(len(db.all()) == 1)
 
+    #print(thing)
+
     after_db = db.all()
     assert after_db.pop() == thing
 
-@given(dictionaries(one_of(text(), characters(), integers()), 
+'''@given(dictionaries(one_of(text(), characters()), 
                     one_of(text(alphabet=one_of(characters(whitelist_categories=('Lo', 'Pf')),
                                                 characters(whitelist_categories=('P')),
                                                 characters(whitelist_categories=('S', 'Z')),
@@ -51,13 +53,36 @@ def test_roundtrip(thing):
                     floats(),
                     integers(),
                     none(),
-                    binary(),
+                    #binary(),
                     lists(elements=one_of(text(), integers()), min_size=2),
                     booleans(),
-                    dictionaries(integers(), text()),
+                    dictionaries(text(), text()),
                     ), min_size=1))
 def test_storage_readwrite(thing):
     storage = JSONStorage('./readwrite.json')
+
+    storage.write(thing)
+
+    assert thing == storage.read()
+    storage.close()'''
+
+
+@given(dictionaries(one_of(text(), characters()), 
+                    one_of(text(alphabet=one_of(characters(whitelist_categories=('Lo', 'Pf')),
+                                                characters(whitelist_categories=('P')),
+                                                characters(whitelist_categories=('S', 'Z')),
+                                                characters(whitelist_categories=('Z')),
+                                                characters(whitelist_categories=('Lt', 'Cf', 'Cc')))),
+                    text(),
+                    floats(allow_nan=False),
+                    integers(),
+                    none(),
+                    lists(elements=one_of(text(), integers()), min_size=2),
+                    booleans(),
+                    dictionaries(text(), text()),
+                    ), min_size=1))
+def test_no_error_json_storage(thing):
+    storage = JSONStorage('./noerrors.json')
 
     storage.write(thing)
 
